b139af5dadc9191d33a5c29c7da90416961f8253
Pascal Lantam Gbate <cyrillantam@gmail.com>
2026-05-21 13:43:43 +0000

    updated my sitemap and robots

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,13 +1,13 @@
 export default async function sitemap() {
   return [
     {
-      url: 'https://yourdomain.com',
+      url: 'https://devforge-tech.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 1.0,
     },
     {
-      url: 'https://yourdomain.com/courses',
+      url: 'https://devforge-tech.vercel.app/dashboard',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.8,
